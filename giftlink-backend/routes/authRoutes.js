4d4d8431421686de029f33de6308447621de1c0a
blakcoder2017 <sherifdata@gmail.com>
2025-05-25 05:16:44 +0000

    add registration backend logic

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,67 @@
+// Step 1 - Task 2: Import necessary packages
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const dotenv = require('dotenv');
+const pino = require('pino');
+
+dotenv.config(); // Load environment variables from .env
+
+const router = express.Router();
+
+// Step 1 - Task 3: Create a Pino logger instance
+const logger = pino();
+
+// Step 1 - Task 4: Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Step 2: Create register endpoint
+router.post('/register', async (req, res) => {
+  try {
+    // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+    const db = await connectToDatabase();
+
+    // Task 2: Access MongoDB collection
+    const collection = db.collection('users');
+
+    // Task 3: Check for existing email
+    const existingEmail = await collection.findOne({ email: req.body.email });
+    if (existingEmail) {
+      return res.status(400).json({ error: 'User with this email already exists' });
+    }
+
+    // Hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(req.body.password, salt);
+
+    // Task 4: Save user details in database
+    const newUser = await collection.insertOne({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: hash,
+      createdAt: new Date(),
+    });
+
+    // Task 5: Create JWT authentication with user._id as payload
+    const payload = {
+      user: {
+        id: newUser.insertedId,
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+
+    logger.info('User registered successfully');
+
+    res.json({ authtoken, email: req.body.email });
+  } catch (e) {
+    logger.error('Internal server error', e);
+    return res.status(500).send('Internal server error');
+  }
+});
+
+
+module.exports = router;
